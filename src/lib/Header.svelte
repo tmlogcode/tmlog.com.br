6ebd8cb968f523e8f34d1df5e216d8769deb3924
diaugust <diaugustdev@gmail.com>
2021-07-19 15:15:22 -0300

    formatação de código

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -9,27 +9,28 @@
 <Navbar />
 
 <div class="w-full flex flex-col lg:flex-row items-center justify-center text-white" id="hero">
-	<div class="wraper flex flex-col items-center lg:flex-row">  
-	<div class="flex flex-col my-8">
-		<h1 class="text-4xl md:text-6xl font-semibold">{$t('hero_slogan')[0].text}</h1>
-		<h1 class="text-4xl md:text-6xl font-semibold">{$t('hero_slogan')[1].text}</h1>
-		<h1 class="text-4xl md:text-6xl font-semibold">{$t('hero_slogan')[2].text}</h1>
-		<h1 class="text-4xl md:text-6xl font-semibold">{$t('hero_slogan')[3].text}</h1>
-		<br>
-		<!-- <p class="font-thin">{$t('hero_slogan')[3].text}</p> -->
-		<br>
-		<a class="flex items-center justify-around w-52 h-14 rounded-full px-2" id="btn" href="#contact">{$t('hero_slogan')[5].text}<img src="/arrow-right.svg" alt=""></a>
-		<br>
+	<div class="wraper flex flex-col items-center lg:flex-row">
+		<div class="flex flex-col my-8">
+			<h1 class="text-4xl md:text-6xl font-semibold">{$t('hero_slogan')[0].text}</h1>
+			<h1 class="text-4xl md:text-6xl font-semibold">{$t('hero_slogan')[1].text}</h1>
+			<h1 class="text-4xl md:text-6xl font-semibold">{$t('hero_slogan')[2].text}</h1>
+			<h1 class="text-4xl md:text-6xl font-semibold">{$t('hero_slogan')[3].text}</h1>
+			<br>
+			<!-- <p class="font-thin">{$t('hero_slogan')[3].text}</p> -->
+			<br>
+			<a class="flex items-center justify-around w-52 h-14 rounded-full px-2" id="btn" href="#contact">{$t('hero_slogan')[5].text}<img src="/arrow-right.svg" alt=""></a>
+			<br>
+		</div>
+		<div>
+			<img src="/hero.png" alt="">
+		</div>
 	</div>
-	<div>
-		<img src="/hero.png" alt="">
-	</div>
-</div>
 </div>
 
 <style>
 	#hero {
 		background-color: #0D0D2B;
+		background-position: center;
 	}
 
 	#btn {
